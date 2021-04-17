38ed74733d3bebfa84cbabb84ef1a546581438e1
Uma Dornala <dornala.umadevi@gmail.com>
2021-04-17 00:29:54 +0000

    I have restored Helloworld.java from second commit

--- Helloworld.java
+++ Helloworld.java
@@ -5,8 +5,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-        // Prints "Hello, Git Repo" to the terminal window.
-        System.out.println("Hello, Git Repo");
     }
 
 }
